583621d994d545fb9b40a06ada2987ae7be41fad
bugmarcus <marcusq@hotmail.com>
2023-09-26 07:51:48 -0300

    Refresh project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-function App() {
+import React from "react";
+import Header from "./components/molecules/Header";
+import "./index.css"; // Importando o CSS global
 
+const App = () => {
   return (
-    <>
-      
-    </>
-  )
-}
+    <div>
+      <Header />
+    </div>
+  );
+};
 
-export default App
+export default App;
